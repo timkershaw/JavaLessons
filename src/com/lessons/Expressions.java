9160da70d235b413700566f2b5e15ab67200edf3
timkershaw <44982931+timkershaw@users.noreply.github.com>
2022-09-22 18:24:18 -0400

    adding the expressions class

--- src/com/lessons/Expressions.java
+++ src/com/lessons/Expressions.java
@@ -6,5 +6,23 @@ public class Expressions {
         // expression = operands & operators
         // operands = values, variables, numbers, quantity
         // operators = + - * / %
+
+        int friends = 10;
+
+        //long way of writing it out
+        friends = friends + 1;
+
+        //short way to increment;
+        int drinks = 15;
+        drinks++;
+
+        //with integer division, we truncate any decimal portion since we like to retain the decimal
+        //25 was an int but need to change data type to a double
+        double shoes = 25;
+        shoes = (double) shoes /3;
+
+        System.out.println(friends);
+        System.out.println(drinks);
+        System.out.println(shoes);
     }
 }
